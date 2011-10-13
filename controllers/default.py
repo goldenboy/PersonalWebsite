b6b3647f058ea04ac1b7e0c25fbf896fe01c07e3
Greg <gregbglw@gmail.com>
2011-10-12 18:19:27 -0700

    First project commit. Basic blog functioning

---
+++ controllers/default.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a samples controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - call exposes all registered services (none by default)
+#########################################################################
+
+POST_SHOW_COUNT = 5
+
+def index():
+    response.menu = [('Home', True, URL('index'), []), ('About', False, URL('about'), []),
+            ('Resume', False, URL('resume'), []), ('Toys', False, URL('toys'), [])]
+    response.title = 'Greg Bigelow'
+    response.meta.author = 'Greg Bigelow'
+
+    # Return the most recent X posts to display on the page.
+    allPosts = db().select(db.posts.ALL, orderby =~ db.posts.date)
+    recentPosts = allPosts[:POST_SHOW_COUNT]
+    return dict(posts = recentPosts)
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request,db)
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+@auth.requires_membership('SuperUser')
+def data():
+    """                                      
+    http://..../[app]/default/data/tables
+    http://..../[app]/default/data/create/[table]
+    http://..../[app]/default/data/read/[table]/[id]
+    http://..../[app]/default/data/update/[table]/[id]
+    http://..../[app]/default/data/delete/[table]/[id]
+    http://..../[app]/default/data/select/[table]
+    http://..../[app]/default/data/search/[table]
+    but URLs bust be signed, i.e. linked with
+      A('table',_href=URL('data/tables',user_signature=True))
+    or with the signed load operator
+      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
+    """
+    return dict(form=crud())
+
+@auth.requires_membership('SuperUser')
+def create_post():
+    """ Write a new blog post. """
+    makePost = crud.create(db.posts, next = URL('index'))
+    return dict(form = makePost)
